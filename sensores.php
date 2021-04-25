a4fefc8146c06e3477873aad45c1629e10209af2
Oscar <oscar.castro@utec.edu.pe>
2021-04-25 18:24:17 -0500

    test php

---
+++ sensores.php
@@ -0,0 +1,27 @@
+<?php
+
+$acelerometro_x = "ax";
+$acelerometro_y = "ay";
+$acelerometro_z = "az";
+
+$orientacion_x = "ox";
+$orientacion_y = "oy";
+$orientacion_z = "oz";
+
+$proximidad = "prox";
+
+$magnetometro_x = "mx";
+$magnetometro_y = "my";
+$magnetometro_z = "mz";
+
+$presion = "presion";
+
+header('Content-Type: application/json');
+
+$data = [
+    'viewCount' => (time() % 10000 *3)
+];
+
+echo json_encode($data);
+
+?>
