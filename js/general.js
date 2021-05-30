a81499b55888521abb2b1d6db642a425e89e9892
Oscar <oscar.castro@utec.edu.pe>
2021-05-30 03:23:36 -0500

    cambio slide bar

--- js/general.js
+++ js/general.js
@@ -19,3 +19,48 @@ function startLiveUpdate (){
 document.addEventListener('DOMContentLoaded', function(){
     startLiveUpdate();
 });
+
+
+var slider1 = document.getElementById("rango-velocidad 1");
+var output1 = document.getElementById("value 1");
+
+output1.innerHTML= slider1.value;
+
+slider1.oninput = function (){
+    output1.innerHTML = this.value;
+}
+var slider2 = document.getElementById("rango-velocidad 2");
+var output2 = document.getElementById("value 2");
+
+output2.innerHTML= slider2.value;
+
+slider2.oninput = function (){
+    output2.innerHTML = this.value;
+}
+
+var slider3 = document.getElementById("rango-velocidad 3");
+var output3 = document.getElementById("value 3");
+
+output3.innerHTML= slider3.value;
+
+slider3.oninput = function (){
+    output3.innerHTML = this.value;
+}
+
+var slider4 = document.getElementById("rango-presion1");
+var output4 = document.getElementById("presion1");
+
+output4.innerHTML= slider4.value;
+
+slider4.oninput = function (){
+    output4.innerHTML = this.value;
+}
+
+var slider5 = document.getElementById("rango-presion2");
+var output5 = document.getElementById("presion2");
+
+output5.innerHTML= slider5.value;
+
+slider5.oninput = function (){
+    output5.innerHTML = this.value;
+}
